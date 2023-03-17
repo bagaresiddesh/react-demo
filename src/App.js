ae3b0dcda9a8e8884923856efdc5ff6128116012
Siddesh Bagare <siddeshb@incubxperts.com>
2023-03-17 12:45:50 +0530

    static ui done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import './App.css';
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Bike Insurance ',
+      amount: 3500,
+      date: new Date(2023,3,5)
+    },
+    {
+      id: 'e2',
+      title: 'Food Bill',
+      amount: 2500,
+      date: new Date(2023,3,7)
+    },
+    {
+      id: 'e3',
+      title: 'Doctor Fees',
+      amount: 950,
+      date: new Date(2023,3,20)
+    },
+    {
+      id: 'e4',
+      title: 'Fuel',
+      amount: '3000',
+      date: new Date(2023,3,25)
+    },
+    {
+      id: 'e5',
+      title: 'SIP',
+      amount: '1000',
+      date: new Date(2023,3,5)
+    },
+  ]
+
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-      <p>
-        Getting Started
-      </p>
-    </div>
+    <Expenses items={expenses} /> 
   );
 }
 
