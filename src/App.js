6696ba8dc10eac5688a309288a45d61103236b0e
Siddesh Bagare <siddeshb@incubxperts.com>
2023-03-20 16:51:43 +0530

    removing dummy data

--- src/App.js
+++ src/App.js
@@ -3,24 +3,6 @@ import NewExpense from "./components/NewExpense/NewExpense";
 import { useState } from "react";
 
 const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'Bike Insurance ',
-    amount: 3500,
-    date: new Date(2023,2,5)
-  },
-  {
-    id: 'e2',
-    title: 'Food Bill',
-    amount: 2500,
-    date: new Date(2023,2,7)
-  },
-  {
-    id: 'e3',
-    title: 'Doctor Fees',
-    amount: 950,
-    date: new Date(2023,2,20)
-  }
 ];
 
 const App = () => {
