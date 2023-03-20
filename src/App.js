1d73ce778b2738dbee009868454c5dca1acfcc69
Siddesh Bagare <siddeshb@incubxperts.com>
2023-03-20 16:50:46 +0530

    done with small demo app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
 
-function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Bike Insurance ',
-      amount: 3500,
-      date: new Date(2023,3,5)
-    },
-    {
-      id: 'e2',
-      title: 'Food Bill',
-      amount: 2500,
-      date: new Date(2023,3,7)
-    },
-    {
-      id: 'e3',
-      title: 'Doctor Fees',
-      amount: 950,
-      date: new Date(2023,3,20)
-    },
-    {
-      id: 'e4',
-      title: 'Fuel',
-      amount: '3000',
-      date: new Date(2023,3,25)
-    },
-    {
-      id: 'e5',
-      title: 'SIP',
-      amount: '1000',
-      date: new Date(2023,3,5)
-    },
-  ]
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Bike Insurance ',
+    amount: 3500,
+    date: new Date(2023,2,5)
+  },
+  {
+    id: 'e2',
+    title: 'Food Bill',
+    amount: 2500,
+    date: new Date(2023,2,7)
+  },
+  {
+    id: 'e3',
+    title: 'Doctor Fees',
+    amount: 950,
+    date: new Date(2023,2,20)
+  }
+];
+
+const App = () => {
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((previousExpense) => {
+      return [expense, ...previousExpense]
+    });
+  };
 
   return (
-    <Expenses items={expenses} /> 
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} /> 
+    </div>
   );
 }
 
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,22 @@
 import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css' 
+import './ExpenseItem.css'
 import Card from '../UI/Card';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
 
     return (
-        <Card className='expense-item'>
-            <ExpenseDate date={props.date} />
-            <div className='expense-item__description '>
-                <h2>
-                    {props.title}
-                </h2>
-            </div>
-            <div className='expense-item__price'>{props.amount}</div>
-        </Card>
+        <li>
+            <Card className='expense-item'>
+                <ExpenseDate date={props.date} />
+                <div className='expense-item__description '>
+                    <h2>
+                        {props.title}
+                    </h2>
+                </div>
+                <div className='expense-item__price'>{props.amount}</div>
+            </Card>
+        </li>
     );
-}
+};
 
 export default ExpenseItem;
